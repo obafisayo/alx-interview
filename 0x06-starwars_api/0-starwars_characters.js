0ff0afc7b9a110182f178debd7ac02fd7063192c
obafisayo <sekaniisjoy@gmail.com>
2024-09-09 20:38:25 +0100

    add 0-star wars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+    console.error('Please provide a movie ID.');
+    process.exit(1);
+}
+
+// Fetch film details
+request(`https://swapi.dev/api/films/${movieId}/`, { json: true }, (err, res, body) => {
+    if (err) {
+        return console.error('Error fetching film details:', err);
+    }
+
+    if (res.statusCode !== 200) {
+        return console.error('Film not found. Status code:', res.statusCode);
+    }
+
+    const characterUrls = body.characters;
+
+    // Fetch characters
+    characterUrls.forEach((url) => {
+        request(url, { json: true }, (err, res, body) => {
+            if (err) {
+                return console.error('Error fetching character details:', err);
+            }
+
+            console.log(body.name);
+        });
+    });
+});
